c97f9fa0ad32a18569e5fc765488ae39d1ecd6a5
Phạm Tiến Phát <phamtienphat2006@gmail.com>
2026-04-11 11:22:20 +0700

    feat: add initial SQL schema for Lab3 database

--- Lab3/Lab3_csdl_xebuyt.sql
+++ Lab3/Lab3_csdl_xebuyt.sql
@@ -12,7 +12,12 @@ INNER JOIN LOTRINH as lt
     ON td.MaTD = lt.MaTD
 WHERE td.DiaDiem LIKE '%HAM NGHI, QUAN 1%'
 --3. Cho biết mã số trạm, tên trạm, loại của các trạm dừng ở Quận 12 thuộc lộ trình của các tuyến xe buýt số 3. (Lưu ý dữ liệu sử dụng tiếng Việt không dấu).
-
+USE csdl_xebuyt;
+SELECT td.MaTD,td.TenTram,td.Loai,lt.MaTB
+FROM TRAMDUNG AS td
+INNER JOIN LOTRINH AS lt
+    ON td.MaTD=lt.MaTD
+WHERE DiaDiem LIKE '%Quan 12%' AND lt.MaTB=3
 --4. Cho biết mã số trạm, tên trạm, loại của các trạm dừng khởi hành của các tuyến xe buýt.
 
 --5. Cho biết tên mã số tuyến, tên tuyến của các tuyến xe buýt có lộ trình đi qua trạm dừng có tên là Chợ Cũ ở Quận 1. (Lưu ý dữ liệu sử dụng tiếng Việt không dấu).
